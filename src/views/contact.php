0328cb4e3d4fd1291d387a5da1128162e1903f47
boruchowitch jennifer <boru_jennifer@hotmail.fr>
2022-10-18 13:51:55 +0200

    formulaire de contact

--- src/views/contact.php
+++ src/views/contact.php
@@ -2,12 +2,46 @@
     <div class="boxContact ">
 
 
-        <div class="aboutText">
-            <h1>About us</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin finibus, augue et placerat pulvinar, augue ipsum efficitur tortor, id volutpat leo dui vitae risus. Vivamus finibus in neque ac tristique. Proin vel accumsan libero. Nunc gravida, dui nec malesuada ornare, libero ipsum dictum lorem, a maximus elit mauris eget quam. Fusce luctus sem quis lorem consequat, sed tincidunt est eleifend. Vestibulum a magna nisl. Aenean ornare hendrerit tristique.
+        <div class="contactText">
+        <div class="formulaire_container">
+                <div class="formulaire_contact">
 
-                Aenean ultrices arcu sed urna fringilla, sed facilisis leo semper. Donec tempus faucibus nisi, ac cursus nisi commodo mollis. Cras consectetur maximus odio ut aliquet. Nunc ut sagittis justo. Aenean in vestibulum mauris. Ut dignissim, quam vel hendrerit fermentum, lacus urna commodo turpis, a pretium nisi nunc in sapien. Praesent pulvinar justo tristique metus blandit, quis rhoncus purus posuere. Pellentesque magna eros, suscipit sit amet luctus in, sagittis placerat dui. Duis tellus tellus, tempus a ipsum eget, mattis interdum urna. Phasellus non massa vel turpis suscipit semper. Praesent lacus massa, pulvinar eu magna eu, varius ultrices sem. Nulla vel leo sit amet tellus varius bibendum vel vitae lacus. Cras luctus iaculis massa non lobortis. Integer non ligula pretium, egestas nunc in, egestas velit. Etiam nec arcu orci. Fusce ante quam, euismod nec interdum et, tincidunt vel nisl.
-            </p>
+                    <form method="post" action="?page=contact">
+                        <div class="formulaire_text_contact">
+                            <h1>Contact</h1>
+                            <p>Keep in touch</p>
+                        </div>
+
+                       <div class="inputData_contact">
+
+                       <div class="inputGroup">
+                            <input class="inputboxContact" type="text" name="email" id="email" placeholder="Name">
+                        </div>
+
+
+                        <div class="inputMessage">
+                        <label for="message"></label>
+                       <textarea class="inputContact" id="message" name="message" rows="3" cols="30" placeholder="Your message..."></textarea> 
+
+                        </div>
+                        
+                       </div>
+                        <!------- Bouton-------->
+                        <div class="cont_act">
+                            <h2>Send</h2>
+                            <input class="btn_contact" type="submit" name="connexion" value="">
+                        </div>                        
+                    </form>
+                    <div class="reseau">
+            <img src="assets\images\illu\facebook.png" alt="">
+            <img src="assets\images\illu\twitter.png" alt="">
+            <img src="assets\images\illu\instagram.png" alt="">
+           </div>
+            
+
+          </div>
+            </div>
+          
         </div>
 
     </div>
