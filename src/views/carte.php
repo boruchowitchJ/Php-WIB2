72d47a48f9b3c3c0b64fea61c76d941a90742267
SoumayaInterface3 <112701196+SoumayaInterface3@users.noreply.github.com>
2022-10-20 00:42:19 +0200

    Update carte.php

--- src/views/carte.php
+++ src/views/carte.php
@@ -7,17 +7,17 @@
 <!-- pour que les picto ouvre les slides. -->
 <div class="container_picto">
 
-<div><a class=" lien picto_atomium"  data-place="atomium" data-id="0"></a> 
+<div><a class=" lien picto_atomium"  data-place="atomium" data-id="9"></a> 
 </div>
-<div><a class=" lien picto_basilique"  data-place="basilique" data-id="0"></a> 
+<div><a class=" lien picto_basilique"  data-place="basilique" data-id="10"></a> 
 </div>
 <div><a class=" lien picto_palaisRoyal"  data-place="palaisRoyal" data-id="7"></a> 
 </div>
 <div><a class=" lien picto_parcRoyal"  data-place="parcRoyal" data-id="4"></a> 
 </div>
-<div><a class=" lien picto_palaisJustice"  data-place="palaisJustice" data-id="0"></a> 
+<div><a class=" lien picto_palaisJustice"  data-place="palaisJustice" data-id="8"></a> 
 </div>
-<div><a class=" lien picto_bourse"  data-place="bourse" data-id="0"></a> 
+<div><a class=" lien picto_bourse"  data-place="bourse" data-id="11"></a> 
 </div>
 <div><a class=" lien picto_grandPlace" href="?page=carte&poi" data-place="grandPlace" data-id="2"></a> 
 </div>
@@ -27,7 +27,7 @@
 </div>
 <div><a class=" lien picto_tourJaponaise"  data-place="tourJaponaise" data-id="3"></a> 
 </div>
-<div><a class=" lien picto_afrique"  data-place="afrique" data-id="0"></a> 
+<div><a class=" lien picto_afrique"  data-place="afrique" data-id="12"></a> 
 </div>
 <div><a class=" lien picto_montDesArts"  data-place="montDesArts" data-id="6"></a> 
 </div>
