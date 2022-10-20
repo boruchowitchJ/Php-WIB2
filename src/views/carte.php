7298c4da1ac4b79d5627fb1b6cae541765522619
Gaelle <g.ikuna@interface3.be>
2022-10-20 14:37:28 +0200

    carte

--- src/views/carte.php
+++ src/views/carte.php
@@ -1,38 +1,37 @@
 <div class="container_carte">
-    <a href="?page=home"><img src="./assets\images\background\Carte-bruxelles1.png" class='carte' alt="carte">  </a>
-      
-   
+    <a href="?page=home"><img src="./assets\images\background\Carte-bruxelles(light bleu).png" class='carte' alt="carte">  </a>
+
+
 
 
 <!-- pour que les picto ouvre les slides. -->
 <div class="container_picto">
 
-<div><a class=" lien picto_atomium"  data-place="atomium" data-id="9"></a> 
+<div><a class=" lien picto_atomium"  data-place="atomium" data-id="9"></a>
 </div>
-<div><a class=" lien picto_basilique"  data-place="basilique" data-id="10"></a> 
+<div><a class=" lien picto_basilique"  data-place="basilique" data-id="10"></a>
 </div>
-<div><a class=" lien picto_palaisRoyal"  data-place="palaisRoyal" data-id="7"></a> 
+<div><a class=" lien picto_palaisRoyal"  data-place="palaisRoyal" data-id="7"></a>
 </div>
-<div><a class=" lien picto_parcRoyal"  data-place="parcRoyal" data-id="4"></a> 
+<div><a class=" lien picto_parcRoyal"  data-place="parcRoyal" data-id="4"></a>
 </div>
-<div><a class=" lien picto_palaisJustice"  data-place="palaisJustice" data-id="8"></a> 
+<div><a class=" lien picto_palaisJustice"  data-place="palaisJustice" data-id="8"></a>
 </div>
-<div><a class=" lien picto_bourse"  data-place="bourse" data-id="11"></a> 
+<div><a class=" lien picto_bourse"  data-place="bourse" data-id="11"></a>
 </div>
-<div><a class=" lien picto_grandPlace" href="?page=carte&poi" data-place="grandPlace" data-id="2"></a> 
+<div><a class=" lien picto_grandPlace" href="?page=carte&poi" data-place="grandPlace" data-id="2"></a>
 </div>
-<div><a class=" lien picto_mannequepis"  data-place="mannequepis" data-id="1"></a> 
+<div><a class=" lien picto_mannequepis"  data-place="mannequepis" data-id="1"></a>
 </div>
-<div><a class=" lien picto_cinquentenaire"  data-place="cinquentenaire" data-id="5"></a> 
+<div><a class=" lien picto_cinquentenaire"  data-place="cinquentenaire" data-id="5"></a>
 </div>
-<div><a class=" lien picto_tourJaponaise"  data-place="tourJaponaise" data-id="3"></a> 
+<div><a class=" lien picto_tourJaponaise"  data-place="tourJaponaise" data-id="3"></a>
 </div>
-<div><a class=" lien picto_afrique"  data-place="afrique" data-id="12"></a> 
+<div><a class=" lien picto_afrique"  data-place="afrique" data-id="12"></a>
 </div>
-<div><a class=" lien picto_montDesArts"  data-place="montDesArts" data-id="6"></a> 
-</div>
-
+<div><a class=" lien picto_montDesArts"  data-place="montDesArts" data-id="6"></a>
 </div>
 
 </div>
 
+</div>
